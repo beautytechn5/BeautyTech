4cb1d872af58425da749ff0356e4ae3d3e4717a2
vayro <nadmj.79@gmail.com>
2026-06-15 21:54:25 +0300

    hardcode supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(
+  'https://cffjcobipldadwsgcwlx.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmZmpjb2JpcGxkYWR3c2djd2x4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMzE0NTcsImV4cCI6MjA2MzYwNzQ1N30.gFriwuZCHItlAFhutPUqZSoplKEGMWqIqoJcBrDOtCQ'
+)
