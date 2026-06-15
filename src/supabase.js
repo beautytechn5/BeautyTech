4b03f3e2f1873304d92da89ddf51f257a4eeed0f
vayro <nadmj.79@gmail.com>
2026-06-15 22:11:18 +0300

    fix supabase key

--- src/supabase.js
+++ src/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://cffjcobipldadwsgcwlx.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmZmpjb2JpcGxkYWR3c2djd2x4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMzE0NTcsImV4cCI6MjA2MzYwNzQ1N30.gFriwuZCHItlAFhutPUqZSoplKEGMWqIqoJcBrDOtCQ'
+  'sb_publishable_FFPAwZjZ4RYd8qESxpWWJA_A_I9ccnD'
 )
